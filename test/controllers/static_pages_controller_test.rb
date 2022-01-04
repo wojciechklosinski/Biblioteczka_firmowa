a40fd00c46af261ebd76829dd32378a539f2b30c
Wojciech Klosinski <wojti357@gmail.com>
2022-01-04 11:45:12 +0100

    Add full title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,21 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
   def setup
-    @base_title = "Firmowa biblioteczka"
+    @base_title = 'Firmowa biblioteczka'
   end
 
-  test "should get root" do
+  test 'should get root' do
     get root_path
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select 'title', "#{@base_title}"
   end
 
-  test "should get about" do
+  test 'should get about' do
     get about_path
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select 'title', "About | #{@base_title}"
   end
-
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,13 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'minitest/reporters'
+Minitest::Reporters.use!
 
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
-    #parallelize(workers: :number_of_processors, with: :threads)
+    # parallelize(workers: :number_of_processors, with: :threads)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all

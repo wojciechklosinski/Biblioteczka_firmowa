5492e2b820e441d1a8b1139bc809c4011bb00139
Wojciech Klosinski <wojti357@gmail.com>
2022-01-04 14:03:26 +0100

    Generate Users controller. Add signup path to button on the home page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 Rails.application.routes.draw do
   root   'static_pages#home'
 
-  get    '/about', to: 'static_pages#about'
+  get    '/about',   to: 'static_pages#about'
+  get    '/signup',  to: 'users#new'
 end

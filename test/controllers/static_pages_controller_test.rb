1c8fc6f341fe1cc95dee6201808d942c23f05e2e
Wojciech Klosinski <wojti357@gmail.com>
2022-01-04 11:08:53 +0100

    Add about page. Add titles and titles tests.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,21 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
+
+  def setup
+    @base_title = "Firmowa biblioteczka"
   end
+
+  test "should get root" do
+    get root_path
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
 end

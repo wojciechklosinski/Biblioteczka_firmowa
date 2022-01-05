f95d3a0c5ae9bedebd6553387cec2a4fa28fe387
Wojciech Klosinski <wojti357@gmail.com>
2022-01-05 15:33:32 +0100

    Add current_user function in sessions helper and change the header layout (depends whether user is logged in). Add dropdown menu.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Niepoprawny email bądź hasło.'
       render 'new'
     end
   end

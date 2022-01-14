68111626fe1e558ee1c70c07c3b900de7d0cbb0c
Wojciech Klosinski <wojti357@gmail.com>
2022-01-14 10:20:55 +0100

    Add typo corrections

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,16 +1,14 @@
 class LoansController < ApplicationController
 	before_action :logged_in_user
 
-  def create
-  	forwarding_url = session[:forwarding_url]  
+  def create 
   	book = Book.find(params[:book_id])
     current_user.borrow(book)
     flash[:success] = "Wypożyczono książkę"
     redirect_to book
   end
 
-  def destroy
-  	forwarding_url = session[:forwarding_url]  
+  def destroy  
   	book = Loan.find(params[:id]).book
   	current_user.update_balance(book)
   	current_user.return(book)

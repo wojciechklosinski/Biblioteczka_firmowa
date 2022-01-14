5e816c550d7f5c410e781c426b382d35314d254e
Wojciech Klosinski <wojti357@gmail.com>
2022-01-14 18:02:06 +0100

    Add seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,37 @@
 # frozen_string_literal: true
 
-# Create a main sample user.
+# Create sample books.
 Book.create!(author: 'Mickiewicz',
              title: 'Dla ciekawskich',
              description: 'Bardzo ciekawa książką, serio.',
-             content: 'Witamy w książce mojej zapraszamy do lektury....',
+             content: 'Witamy w naszej książce. Zapraszamy do lektury....',
              price: 10)
 
-# Generate a bunch of additional users.
+Book.create!(author: 'Juliusz Słowacki',
+             title: 'Konrad Wallenrood',
+             description: 'Słowacki wspaniałym poetą był.',
+             content: 'Słowacki jest lepszy niż Mickiewicz!!!!',
+             price: 5)
+
+Book.create!(author: 'Wojtek',
+             title: 'Jak zostałem koderem.',
+             description: 'Przygoda Wojtka z krainy dżemów. Droga do sławy na stackoverflow.',
+             content: 'Wszystko zaczęło się na Mechatronice...',
+             price: 23)
+
+Book.create!(author: 'Gombrowicz',
+             title: 'Ferdydurke',
+             description: 'O łydkach i upupianiu.',
+             content: 'Harcerki mają grube łydki.',
+             price: 1)
+
+Book.create!(author: 'Dostojewski',
+             title: 'Zbrodnia Ikara',
+             description: 'Książka o Ikarze, który popełnił zbrodnię!',
+             content: 'Dawno dawno temu, na lekcji Polskiego...',
+             price: 10)
+
+# Generate a bunch of additional books.
 99.times do |_n|
   name  = Faker::Name.name
   title = Faker::Lorem.words(number: rand(2..5)).join(' ')
@@ -20,17 +44,18 @@ Book.create!(author: 'Mickiewicz',
                price: 2)
 end
 
-User.create!(name: 'Example User',
-             email: 'example@railstutorial.org',
-             password: 'foobar',
-             password_confirmation: 'foobar',
+# Create admin user.
+User.create!(name: 'Admin User',
+             email: 'admin@admin.pl',
+             password: 'admin123',
+             password_confirmation: 'admin123',
              admin: true,
-             balance: 20)
+             balance: 0)
 
-# Generate a bunch of additional books.
+# Generate a bunch of additional users.
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n + 1}@railstutorial.org"
+  email = "example-#{n + 1}@firmowabiblioteczka.org"
   password = 'password'
   User.create!(name: name,
                email: email,
@@ -39,13 +64,13 @@ User.create!(name: 'Example User',
                balance: 10)
 end
 
-# Loans
+# Generate sample loans for admin user.
 books = Book.all
 user  = User.first
-borrowed = books[2..20]
+borrowed = books[2..10]
 borrowed.each { |book| user.borrow(book) }
 
-# Historical loans
+# Generate historical loans for admin user.
 user = User.first
 50.times do
   name  = Faker::Name.name
